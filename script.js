100f6f5a874033e41dded0d06af71b60d602f7f2
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-20 07:22:27 +0100

    16x16 divs created and placed within grid-container

---
+++ script.js
@@ -0,0 +1,12 @@
+(() => {
+    
+
+    for (i = 0; i < 256; i++) {
+    const newDiv = document.createElement("div")
+    const withinContainer = document.getElementById('gridContainer')
+    
+    withinContainer.insertAdjacentElement('afterbegin', newDiv)
+    }
+})();
+
+
