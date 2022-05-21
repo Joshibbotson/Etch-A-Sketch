8c0b3c4b126b0e74ddb699428b943b2b6626eea1
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-21 09:30:50 +0100

    grid now adjusts according to slider value.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-(() => {
-    
-
-    for (i = 0; i < 256; i++) {
-    const newDiv = document.createElement("div")
-    const withinContainer = document.getElementById('gridContainer')
-    
-    withinContainer.insertAdjacentElement('afterbegin', newDiv)
-    }
-})();
+const gridSizeSlider = document.getElementById('slider'); // value slider//
+const grid = document.getElementById('gridContainer')
 
 
+gridSizeSlider.addEventListener('click', modifyGridSize ) // when clicked value slider//
+
+function modifyGridSize () {
+    resetGrid();
+    gridCss();
+    let gridSize = gridSizeSlider.value * gridSizeSlider.value;
+
+    console.log(gridSize)
+        
+    for (i = 0; i < gridSize; i++) {
+        const newDiv = document.createElement("div")
+        newDiv.setAttribute('id', "square");
+        newDiv.setAttribute('class', "square")
+        
+        grid.insertAdjacentElement('afterbegin', newDiv)
+        }
+    gridSize = 0;
+    console.log (gridSize)
+}
+
+function resetGrid () { //removes any created divs, thus resetting the grid clean//
+    grid.innerHTML = '';
+}
+
+function gridCss () { //alters the css grid sizing//
+
+    let size = gridSizeSlider.value;
+
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateR = `repeat(${size}, 1fr)`
+
+}
