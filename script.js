09b02f1177208f1a3d3d3afb5317507ebc126ea3
Joshua Ibbotson <josh@Joshuas-MacBook-Pro-2.local>
2022-05-23 19:54:02 +0100

    Most of the basics work, things can be drawn, colours can be changed, but rainbow mode doesn't work, possibly the methodology as a whole for how this works should be changed in relation to applying ink to canvas (altering div colour on click)

--- script.js
+++ script.js
@@ -1,36 +1,107 @@
 const gridSizeSlider = document.getElementById('slider'); // value slider//
-const grid = document.getElementById('gridContainer')
+const grid = document.getElementById('gridContainer');
+const div = document.getElementById('square');
+
+let colourValue = "black"
+
+//html buttons//
+const clearBtn = document.getElementById('clearBtn').addEventListener('click', (e) => {
+    resetGrid()
+    modifyGridSize()
+})
+const eraserBtn = document.getElementById('eraserBtn').addEventListener('click', (e) => {
+    colourValue = "white"
+})
+const rainbowBtn = document.getElementById('rainbowBtn').addEventListener('click', (e) => {
+    
+    let r = Math.floor(Math.random()*255)
+    let g = Math.floor(Math.random()*255)
+    let b = Math.floor(Math.random()*255)
+
+    return colourValue = `rgb(${r}, ${g}, ${b})`
+})
+const colourBtn = document.getElementById('colourBtn').addEventListener ('click', (e) => {
+    colourValue = colourChoice.value;
+} )
+const colourChoice = document.getElementById('colourChoice')
 
 
-gridSizeSlider.addEventListener('click', modifyGridSize ) // when clicked value slider//
+colourChoice.addEventListener('input', function defineColourChoice(){
+    return colourValue = colourChoice.value
+});
+
+let mousedown = false;
+// let mouseover = false; // not sure about this yet
+grid.addEventListener('mousedown', mousedownSwitch)
+grid.addEventListener('mouseoff', mousedownSwitch)
+
+function mousedownSwitch(e) {
+    if (e.type === 'mousedown' && mousedown === false){
+        return mousedown = true
+    }
+        return mousedown = false
+}
+
+// function mouseoffSwitch (e) { // not sure about this yet.
+//     if (e.type === 'mouseoff' && mousedown === true) {
+//         return mouseover = true;
+//     }
+//     return mouseover = false;
+// }
+
+for (i = 0; i < 16*16; i++) {
+    const newDiv = document.createElement("div")
+    newDiv.setAttribute('id', "square");
+    newDiv.setAttribute('class', "square")
+    
+    grid.insertAdjacentElement('afterbegin', newDiv)
+    
+
+    newDiv.addEventListener('mouseover', divColourChange)
+    newDiv.addEventListener('mousedown', divColourChange)
+    }
+
+gridSizeSlider.addEventListener('input', modifyGridSize ) // when input changes on value slider reset the grid//
+
+
 
 function modifyGridSize () {
     resetGrid();
     gridCss();
-    let gridSize = gridSizeSlider.value * gridSizeSlider.value;
 
-    console.log(gridSize)
+    let gridSize = gridSizeSlider.value * gridSizeSlider.value;
         
     for (i = 0; i < gridSize; i++) {
-        const newDiv = document.createElement("div")
+        let newDiv = document.createElement("div")
         newDiv.setAttribute('id', "square");
         newDiv.setAttribute('class', "square")
+        
+        newDiv.addEventListener('mouseover', divColourChange)
+        newDiv.addEventListener('mousedown', divColourChange)
+        newDiv.addEventListener('mouseoff', divColourChange)
         
         grid.insertAdjacentElement('afterbegin', newDiv)
         }
     gridSize = 0;
-    console.log (gridSize)
 }
 
-function resetGrid () { //removes any d the grid clean//
+function resetGrid () { //removes any divs thus wiping the grid clean//
     grid.innerHTML = '';
+    gridCss;
 }
 
 function gridCss () { //alters the css grid sizing//
-
     let size = gridSizeSlider.value;
 
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
 }
+function divColourChange (e) {
+    if (e.type === "mouseover" && !mousedown) {
+        return
+    }
+    // if (e.type === "mouseover" && e.type === "mousedown" && mousedown === true){
+    e.target.style.backgroundColor =`${colourValue}`;
+
+};
